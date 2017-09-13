7566ac1da65435ffc0e25bf19b7db592d9f60014
Travis Builder <me@andrepolischuk.com>
2017-09-13 10:43:05 +0000

    Update 2017-09-13T10:43:03.694Z

---
+++ sw.js
@@ -0,0 +1,59 @@
+/* eslint-env serviceworker */
+/* global fetch URL */
+'use strict'
+const cacheVersion = 'v1'
+
+const assetsForCache = [
+  './offline.html',
+  './index.js',
+  './styles.css',
+  './favicon.png'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches
+      .open(cacheVersion)
+      .then(cache => cache.addAll(assetsForCache))
+  )
+})
+
+self.addEventListener('activate', event => {
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url)
+
+  if (event.request.method === 'GET') {
+    if (url.origin === self.location.origin && url.pathname === '/') {
+      event.respondWith(
+        caches
+          .open(cacheVersion)
+          .then(cache => fetch(event.request)
+            .then(networkResponse => {
+              cache.put(event.request, networkResponse.clone())
+              return networkResponse
+            })
+            .catch(() => cache.match(event.request)
+              .then(cachedResponse => cachedResponse || cache.match('./offline.html'))
+            )
+          )
+      )
+    } else {
+      event.respondWith(
+        caches
+          .open(cacheVersion)
+          .then(cache => cache.match(event.request)
+            .then(cachedResponse => cachedResponse || fetch(event.request)
+              .then(networkResponse => {
+                cache.put(event.request, networkResponse.clone())
+                return networkResponse
+              })
+            )
+            .catch(() => cache.match('./offline.html'))
+          )
+      )
+    }
+  }
+})
