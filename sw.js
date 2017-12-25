18e8bc8b4b7e5851deab56789e7b7613baa08e4c
Travis Builder <me@andrepolischuk.com>
2017-12-25 15:59:38 +0000

    Update 2017-12-25T15:59:37.217Z

--- sw.js
+++ sw.js
@@ -28,32 +28,40 @@ self.addEventListener('fetch', event => {
   if (event.request.method === 'GET') {
     if (url.origin === self.location.origin && url.pathname === '/') {
       event.respondWith(
-        caches
-          .open(cacheVersion)
-          .then(cache => fetch(event.request)
-            .then(networkResponse => {
-              cache.put(event.request, networkResponse.clone())
-              return networkResponse
-            })
-            .catch(() => cache.match(event.request)
-              .then(cachedResponse => cachedResponse || cache.match('./offline.html'))
-            )
+        update(event.request)
+          .catch(() => fromCache(event.request)
+            .then(response => response || fromCache('./offline.html'))
           )
       )
     } else {
       event.respondWith(
-        caches
-          .open(cacheVersion)
-          .then(cache => cache.match(event.request)
-            .then(cachedResponse => cachedResponse || fetch(event.request)
-              .then(networkResponse => {
-                cache.put(event.request, networkResponse.clone())
-                return networkResponse
-              })
-            )
-            .catch(() => cache.match('./offline.html'))
+        fromCache(event.request)
+          .then(response => response || update(event.request)
+            .catch(() => fromCache('./offline.html'))
           )
+      )
+
+      event.waitUntil(
+        fromCache(event.request)
+          .then(response => response && update(event.request))
       )
     }
   }
 })
+
+function fromCache (request) {
+  return caches
+    .open(cacheVersion)
+    .then(cache => cache.match(request))
+}
+
+function update (request) {
+  return caches
+    .open(cacheVersion)
+    .then(cache => fetch(request)
+      .then(response => {
+        cache.put(request, response.clone())
+        return response
+      })
+    )
+}
